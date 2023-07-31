1027ac2993572686d75d722ae006d83a579cebc0
Valerie Freeman <valerah7@gmail.com>
2023-07-31 15:53:33 -0500

    wip

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-import { TypeList } from "./TypeList.js";
 import { SchoolList } from "./SchoolList.js";
+import { TypeList } from "./TypeList.js";
 
 const mainContainer = document.querySelector("#container");
 
@@ -7,8 +7,8 @@ const applicationHTML = `
   <h1 id="title">Goblin Librarian</h1>
   <div id="body">
       <div id="lists_container">
-        ${SchoolList()}
-        ${TypeList()}
+      ${SchoolList()}
+      ${TypeList()}
       </div>
       <div id="details_container">
         <div id="message">Please select a Magician Type or School of magic to show details.</div>
